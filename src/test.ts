c760590e4a4cddb364016ebad62decaf1ce74ebf
felis <felisicca@gmail.com>
2025-03-04 16:52:40 -0300

    inclusion de testeo de la clase User y sus metodos

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,7 @@
 import test from "ava";
 import { Product } from "./index";
+import { User } from "./index";
+
 
 test("Testeo la clase Product", (t) => {
   // test Product class
@@ -7,4 +9,11 @@ test("Testeo la clase Product", (t) => {
   t.is(product.name, "Product 1");
   t.is(product.price, 10);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+test("Testeo la clase User y sus métodos", (t) => {
+  // test User class y sus métodos
+  const user = new User("Alejandro")
+  const product = new Product("Product 2", 10)
+  user.addProduct(product)
+  t.is(user.products.length, 1)
+  t.is(user.products[0].name, "Product 2")
+});
